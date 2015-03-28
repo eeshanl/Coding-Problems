03b4414fe9fce47ba50dec6d824d313f57e2f30a
eeshanl <eeshanl@gmail.com>
2015-03-27 17:32:05 -0700

    edits

--- IsUnique.java
+++ IsUnique.java
@@ -6,21 +6,20 @@
 import java.util.*;
 
 public class IsUnique {
-      
+
    public static void main(String[] args) {
       String str = "blah"; // Edit this string
       boolean unique = isUnique(str);
       System.out.println("Is this string unique?: " + unique);
    }
-   
-   
+
    public static boolean isUnique(String str) {
       HashSet<Character> uniqueSet = new HashSet<Character>();
       for (int i = 0; i < str.length(); i++) {
          if (!uniqueSet.contains(str.charAt(i))) {
             uniqueSet.add(str.charAt(i));
          } else {
-            return false;   
+            return false;
          }
       }
       return true;
