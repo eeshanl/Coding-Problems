488fb6ae23747476b7498264065a0db3740c617c
eeshanl <eeshanl@gmail.com>
2015-03-28 18:00:11 -0700

    edits

--- removeDupsLinkedList/removeDups.java
+++ removeDupsLinkedList/removeDups.java
@@ -3,7 +3,7 @@ public class removeDups {
    public static void main(String[] args) {
       Node front = new Node(1);
       Node temp = front;
-      front.next = new Node(2);
+      temp.next = new Node(2);
       temp = temp.next;
       temp.next = new Node(3);
       temp = temp.next;
@@ -19,8 +19,10 @@ public class removeDups {
       temp = temp.next;
       temp.next = new Node(2);
       temp = front;
+      System.out.print("List with duplicates: ");
       printList(temp);
       removeDups(temp);
+      System.out.print("\nList without duplicates: ");
       printList(temp);
    }
    
@@ -29,7 +31,13 @@ public class removeDups {
    }
    
    public static void printList(Node front) {
-   
+      Node cur = front;
+      System.out.print("[");
+      while (cur.next.next != null) {
+         System.out.print(cur.data + ", ");
+         cur = cur.next;
+      }
+      System.out.println(cur.next.data + "]");
    }
 
 }
