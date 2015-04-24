d7df8a4f2fb7974ee506e18f78d71461769a726b
eeshanl <eeshan@uw.edu>
2015-04-24 01:43:24 -0700

    bst

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5,6,7,8,9};
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(binarySearch(nums, nums[i]) + " "); // should print 1 2 3 4 5 6 7 8 9
+		}	
+	}
+
+	public static int binarySearch(int[] nums, int x) {
+		return binarySearch(nums, x, 0, nums.length - 1);
+	}
+
+	private static int binarySearch(int[] nums, int x, int low, int high) {
+		if (low > high) {
+			return -1;
+		}
+		int mid = (low + high) / 2;
+		if (nums[mid] == x) {
+			return x;
+		} else if (nums[mid] < x) {
+			return binarySearch(nums, x, mid + 1, high);
+		} else {
+			return binarySearch(nums, x, low, mid - 1);
+		}
+	}
+
+}
