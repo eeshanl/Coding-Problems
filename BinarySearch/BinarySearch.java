3ebb3dac0576e578684ac5771bbc6dd05aff73c0
eeshanl <eeshan@uw.edu>
2015-04-24 01:46:09 -0700

    binarysearch

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,4 +1,8 @@
+// Eeshan Londhe
+// Binary search algorithm with O(log(n)) time complexity
+
 public class BinarySearch {
+
 
 	public static void main(String[] args) {
 		int[] nums = {1,2,3,4,5,6,7,8,9};
@@ -7,10 +11,12 @@ public class BinarySearch {
 		}	
 	}
 
+	// binary search algorithm
 	public static int binarySearch(int[] nums, int x) {
 		return binarySearch(nums, x, 0, nums.length - 1);
 	}
 
+	// helper method for binary search
 	private static int binarySearch(int[] nums, int x, int low, int high) {
 		if (low > high) {
 			return -1;
