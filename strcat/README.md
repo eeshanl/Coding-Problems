2fb84a727060f962f6d89656d655047c19f2954b
eeshanl <eeshan@uw.edu>
2015-04-26 04:25:29 -0700

    strcat

---
+++ strcat/README.md
@@ -0,0 +1,2 @@
+# String concatenation (in C)
+* Combines two strings into one and returns it
---
+++ strcat/strcat.c
@@ -0,0 +1,32 @@
+// My implementation of the strcat function in C
+
+#include "stdio.h" // for printf
+#include "stdlib.h" // for malloc
+
+// Passes in two char* ptrs
+// returns a new char* ptr that contains the concatenation of the 2 strings
+char* strCat(char* one, char* two) {
+  char* cat = (char*) malloc(sizeof(one) + sizeof(two) - 1);
+  char* catPtr = cat;
+  char* onePtr = one;
+  char* twoPtr = two;
+  while (*onePtr) {
+    *catPtr = *onePtr;
+    catPtr++;
+    onePtr++;
+  }
+  while (*twoPtr) {
+    *catPtr = *twoPtr;
+    catPtr++;
+    twoPtr++;
+  }
+
+  return cat;
+}
+
+int main() {
+  char one[4] = {'O', 'n', 'e', '\0'};
+  char two[4] = {'T', 'w', 'o', '\0'};
+  char* result = strCat(one,two);
+  printf("%s\n", result);
+}
