7ccad13c5521d4e6787f4cbeb888252d4764802a
eeshanl <eeshan@uw.edu>
2015-04-26 03:46:27 -0700

    fib

--- FibonacciSeries/fibonacci.c
+++ FibonacciSeries/fibonacci.c
@@ -3,28 +3,28 @@
 #include "stdio.h" // printf & fprintf
 #include "stdlib.h" // for atoi
 
-void printFib(int n) {
-  for (int i = 0; i < n; i++) {
-    static int first = 0;
-    static int second = 1;
-    static int next = 1;
+void printFib(unsigned long int n) {
+  for (unsigned long int i = 0; i < n; i++) {
+    static unsigned long int first = 0;
+    static unsigned long int second = 1;
+    static unsigned long int next = 1;
     if (i == n - 1) {
-      printf("%d\n", next);
+      printf("%lu\n", next);
     } else {
-      printf("%d ", next);
+      printf("%lu ", next);
     }
     next = first + second;
     first = second;
     second = next;
   }
-}a
+}
 
 int main(int argc, char** argv) {
   if (argc < 2) {
     fprintf(stderr, "enter arg\n");
     exit(1);
   }
-  int n = atoi(argv[1]);
+  unsigned long int n = atoi(argv[1]);
   printFib(n);
   return 0;
 }
