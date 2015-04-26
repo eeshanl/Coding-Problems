483a8f24f0a1623a953ab703917919e966e9938f
eeshanl <eeshan@uw.edu>
2015-04-25 18:25:20 -0700

    edits

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,11 +1,12 @@
 /* Eeshan Londhe
  * Program to check if a given string is a palindrome.
+ * Ignores spaces in the given string.
  */
 
 public class Palindrome {
 
    public static void main (String[] args) {
-      String pal = "racecar"; // racecar is a common palindrome
+      String pal = "rac eca r"; // racecar is a common palindrome
       System.out.println("Is palindrome?: " + isPal(pal));
    }
 
@@ -19,6 +20,11 @@ public class Palindrome {
       } else {
          if (s.charAt(i) == s.charAt(j)) {
             return true && isPal(s, i+1, j-1);
+            
+         } else if (s.charAt(i) == ' ') {
+            return isPal(s, i+1, j);
+         } else if (s.charAt(j) == ' ') {
+            return isPal(s, i, j-1);
          } else {
             return false;
          }
---
+++ Palindrome/README.md
@@ -0,0 +1,2 @@
+#isPalindrome()
+* Program to check if a given string is a palindrome. (ignores spaces)
